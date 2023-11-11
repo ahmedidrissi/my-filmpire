06ebec9a209034f1dd512a94be2b59e708d3ec2b
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-12 00:35:49 +0100

    Refactor API key handling and update default search term.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { useState, useEffect } from "react";
-import { API_KEY } from "../.env/keys";
+import { useEffect } from "react";
 
+const API_KEY = process.env.REACT_APP_API_KEY;
 const API_URL = "http://www.omdbapi.com/?apikey=" + API_KEY;
 
 function App() {
-
   const searchMovies = async (title) => {
     const res = await fetch(`${API_URL}&s=${title}`);
     const data = await res.json();
@@ -12,7 +11,7 @@ function App() {
   };
 
   useEffect(() => {
-    searchMovies("batman");
+    searchMovies("Avengers");
   }, []);
 
   return <div>App</div>;
